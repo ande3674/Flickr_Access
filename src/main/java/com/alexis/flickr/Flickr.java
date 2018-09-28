4d1afb90da7e2640381ba40baefa62c18d2bfc16
Alexis <email@email.com>
2018-09-27 20:11:25 -0500

    add key string

--- src/main/java/com/alexis/flickr/Flickr.java
+++ src/main/java/com/alexis/flickr/Flickr.java
@@ -15,6 +15,7 @@ public class Flickr {
 
     String URL = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=41014e55b2a616bc84ba31aa9c8966cb&per_page=10&format=json&nojsoncallback=1";
     String secret = "ec70c827417a7332";
+    String key = "41014e55b2a616bc84ba31aa9c8966cb";
 
     public Flickr(){}
 
@@ -24,11 +25,13 @@ public class Flickr {
 
             HttpResponse<JsonNode> response = Unirest.get(URL).header("accept", "application/json").asJson();
             JSONObject jsonObject = response.getBody().getObject();
-            /*
+
+            /* JSON stuff from my old project
             JSONArray items = jsonObject.getJSONArray("items");
             JSONObject itemsObj = items.getJSONObject(0);
             JSONObject volInfoObj = itemsObj.getJSONObject("volumeInfo");
             */
+
             return  jsonObject.toString();
 
         }
